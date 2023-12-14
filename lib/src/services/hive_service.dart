aff9ee1b3a556ccddc32c0ef6b0165f03cc9af38
RIDVANSIT <99076569+RIDVANSIT@users.noreply.github.com>
2023-12-14 16:22:25 +0300

    first commit

---
+++ lib/src/services/hive_service.dart
@@ -0,0 +1,32 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+import '../models/person_model.dart';
+
+// typedef
+class HiveService {
+  static Future<bool> initService() async {
+    await Hive.initFlutter();
+    Hive.registerAdapter(PersonModelAdapter());
+    return true;
+  }
+
+  static Future<Box<T>> openBox<T>() async {
+    if (Hive.isBoxOpen('$T')) {
+      return Hive.box<T>('$T');
+    } else {
+      return Hive.openBox<T>('$T');
+    }
+  }
+
+  static Future<int> addToBox<T>(Box<T> box, T data) {
+    return box.add(data);
+  }
+
+  static Future<void> remove<T>(Box<T> box, int index) {
+    return box.deleteAt(index);
+  }
+} 
+
+
+// hive, hive_flutter => deps
+// hive_generator, build_runner => dev deps
